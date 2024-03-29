72db3a0d9e7f62acfd27dabb2e0a544e699ca8a6
DESKTOP-D44ERO5\Aditya <not-my-email@not-a-valid-domain.com>
2024-03-29 19:26:04 -0400

    Initial commit

---
+++ src/main/java/com/aditya/java/tree/TreeDFS.java
@@ -0,0 +1,39 @@
+package com.aditya.java.tree;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+
+import com.aditya.java.tree.pojo.Node;
+import com.aditya.java.tree.util.TreeUtil;
+
+public class TreeDFS {
+
+	public static void main(String args[]) {
+		String[] array = { "1", "2", "3", "4", "5", "6", "7", "8" };
+		Node<String> treeRoot = TreeUtil.createTreeFromArray(array);
+		List<String> dfs = iterativeDFS(treeRoot);
+		System.out.println(dfs);
+	}
+
+	static <T> List<T> iterativeDFS(Node<T> treeRoot) {
+		List<T> toReturn = new LinkedList<>();
+		
+		Stack<Node<T>> dfsStack = new Stack<>();
+		dfsStack.push(treeRoot);
+
+		do {
+			Node<T> currentNode = dfsStack.pop();
+			if (currentNode.left != null) {
+				dfsStack.push(currentNode.left);
+			}
+			if (currentNode.right != null) {
+				dfsStack.push(currentNode.right);
+			}
+			toReturn.add(currentNode.data);
+		} while (!dfsStack.isEmpty());
+		return toReturn;
+	}
+
+}
---
+++ src/main/java/com/aditya/java/tree/pojo/Node.java
@@ -0,0 +1,35 @@
+package com.aditya.java.tree.pojo;
+
+import java.util.Objects;
+
+public class Node<T> {
+	public T data;
+	public Node<T> left;
+	public Node<T> right;
+
+	public Node(T data) {
+		super();
+		this.data = data;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(data, left, right);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Node<?> other = (Node<?>) obj;
+		return Objects.equals(data, other.data) && Objects.equals(left, other.left)
+				&& Objects.equals(right, other.right);
+	}
+
+	
+	
+}
