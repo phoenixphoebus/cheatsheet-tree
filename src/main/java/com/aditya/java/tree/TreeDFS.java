29bb86493409f812c9fd3eae3f5685c88e2f52aa
Phoenix Phoebus <151787300+phoenixphoebus@users.noreply.github.com>
2024-03-30 23:30:49 -0400

    will this work? (#2)
    Co-authored-by: DESKTOP-D44ERO5\Aditya <not-my-email@not-a-valid-domain.com>

--- src/main/java/com/aditya/java/tree/TreeDFS.java
+++ src/main/java/com/aditya/java/tree/TreeDFS.java
@@ -10,30 +10,30 @@ import com.aditya.java.tree.util.TreeUtil;
 
 public class TreeDFS {
 
-	public static void main(String args[]) {
-		String[] array = { "1", "2", "3", "4", "5", "6", "7", "8" };
-		Node<String> treeRoot = TreeUtil.createTreeFromArray(array);
-		List<String> dfs = iterativeDFS(treeRoot);
-		System.out.println(dfs);
-	}
+    public static void main(String args[]) {
+        String[] array = { "1", "2", "3", "4", "5", "6", "7", "8" };
+        Node<String> treeRoot = TreeUtil.createTreeFromArray(array);
+        List<String> dfs = iterativeDFS(treeRoot);
+        System.out.println(dfs);
+    }
 
-	static <T> List<T> iterativeDFS(Node<T> treeRoot) {
-		List<T> toReturn = new LinkedList<>();
-		
-		Stack<Node<T>> dfsStack = new Stack<>();
-		dfsStack.push(treeRoot);
+    static <T> List<T> iterativeDFS(Node<T> treeRoot) {
+        List<T> toReturn = new LinkedList<>();
 
-		do {
-			Node<T> currentNode = dfsStack.pop();
-			if (currentNode.left != null) {
-				dfsStack.push(currentNode.left);
-			}
-			if (currentNode.right != null) {
-				dfsStack.push(currentNode.right);
-			}
-			toReturn.add(currentNode.data);
-		} while (!dfsStack.isEmpty());
-		return toReturn;
-	}
+        Stack<Node<T>> dfsStack = new Stack<>();
+        dfsStack.push(treeRoot);
+
+        do {
+            Node<T> currentNode = dfsStack.pop();
+            if (currentNode.left != null) {
+                dfsStack.push(currentNode.left);
+            }
+            if (currentNode.right != null) {
+                dfsStack.push(currentNode.right);
+            }
+            toReturn.add(currentNode.data);
+        } while (!dfsStack.isEmpty());
+        return toReturn;
+    }
 
 }
--- src/main/java/com/aditya/java/tree/TreeInorder.java
+++ src/main/java/com/aditya/java/tree/TreeInorder.java
@@ -7,28 +7,27 @@ import com.aditya.java.tree.pojo.Node;
 import com.aditya.java.tree.util.TreeUtil;
 
 public class TreeInorder {
-	
-	public static void main(String args[]) {
-		String[] array = { "1", "2", "3", "4", "5", "6", "7", "8" };
-		Node<String> treeRoot = TreeUtil.createTreeFromArray(array);
-		List<String> inorder = recursiveInorder(treeRoot);
-		System.out.println(inorder);
-	}
-	
-	static <T> List<T> recursiveInorder(Node<T> treeRoot){
-		List<T> toReturn = new LinkedList<>();
-		inorder(treeRoot, toReturn);
-		return toReturn;
-	}
 
-	
-	private static <T> void inorder(Node<T> node, List<T> list) {
-		if(node.left!=null) {
-			inorder(node.left, list);
-		}
-		list.add(node.data);
-		if(node.right!=null) {
-			inorder(node.right, list);
-		}
-	}
+    public static void main(String args[]) {
+        String[] array = { "1", "2", "3", "4", "5", "6", "7", "8" };
+        Node<String> treeRoot = TreeUtil.createTreeFromArray(array);
+        List<String> inorder = recursiveInorder(treeRoot);
+        System.out.println(inorder);
+    }
+
+    static <T> List<T> recursiveInorder(Node<T> treeRoot) {
+        List<T> toReturn = new LinkedList<>();
+        inorder(treeRoot, toReturn);
+        return toReturn;
+    }
+
+    private static <T> void inorder(Node<T> node, List<T> list) {
+        if (node.left != null) {
+            inorder(node.left, list);
+        }
+        list.add(node.data);
+        if (node.right != null) {
+            inorder(node.right, list);
+        }
+    }
 }
